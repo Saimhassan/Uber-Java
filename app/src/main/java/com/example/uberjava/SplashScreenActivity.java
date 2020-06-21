b4863f8c7a7ec178113664088159f866cfbd0406
Saim <saimh355@gmail.com>
2020-06-22 01:37:06 +0500

    Resizing Drawable image, Add rxjava code with time delay for splash screen in java code.

--- app/src/main/java/com/example/uberjava/SplashScreenActivity.java
+++ app/src/main/java/com/example/uberjava/SplashScreenActivity.java
@@ -3,6 +3,13 @@ package com.example.uberjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Completable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Action;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +17,18 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        delaySplashScreen();
+
+    }
+
+    private void delaySplashScreen() {
+        Completable.timer(5,TimeUnit.SECONDS,
+                AndroidSchedulers.mainThread())
+                .subscribe(new Action() {
+                    @Override
+                    public void run() throws Exception {
+                        Toast.makeText(SplashScreenActivity.this, "Splash Screen done", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
