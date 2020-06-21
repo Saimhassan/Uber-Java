a50e957b50e9c090fe032809ae8a275755ff7079
Saim <saimh355@gmail.com>
2020-06-22 01:18:48 +0500

    Add SplashScreen Theme to the Sytle.xml

--- app/src/main/java/com/example/uberjava/SplashScreenActivity.java
+++ app/src/main/java/com/example/uberjava/SplashScreenActivity.java
@@ -9,6 +9,6 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
     }
 }
