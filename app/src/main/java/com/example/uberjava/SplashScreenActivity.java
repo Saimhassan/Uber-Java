51db03199a4c3b2304a251f550200f8d213e06da
Saim <saimh355@gmail.com>
2020-06-25 18:13:45 +0500

    Write code for Phone and Google Login

--- app/src/main/java/com/example/uberjava/SplashScreenActivity.java
+++ app/src/main/java/com/example/uberjava/SplashScreenActivity.java
@@ -1,10 +1,21 @@
 package com.example.uberjava;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthMethodPickerLayout;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthAnonymousUpgradeException;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Completable;
@@ -13,11 +24,46 @@ import io.reactivex.functions.Action;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    private final static int LOG_IN_REQUEST_CODE = 7171;
+    private List<AuthUI.IdpConfig> providers;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener listener;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        delaySplashScreen();
+        init();
+
+    }
+
+    private void init() {
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+        firebaseAuth = FirebaseAuth.getInstance();
+        listener = myFirebaseAuth->{
+            FirebaseUser user = myFirebaseAuth.getCurrentUser();
+            if (user != null)
+                delaySplashScreen();
+            else
+                showLogInLayout();
+        };
+    }
+
+    private void showLogInLayout() {
+        AuthMethodPickerLayout authMethodPickerLayout = new AuthMethodPickerLayout
+                .Builder(R.layout.layout_sign_in)
+                .setPhoneButtonId(R.id.btn_phone_sign_in)
+                .setGoogleButtonId(R.id.btn_email_sign_in)
+                .build();
+        startActivityForResult(AuthUI.getInstance()
+        .createSignInIntentBuilder()
+        .setAuthMethodPickerLayout(authMethodPickerLayout)
+        .setIsSmartLockEnabled(false)
+        .setAvailableProviders(providers)
+        .build(),LOG_IN_REQUEST_CODE);
 
     }
 
@@ -30,5 +76,22 @@ public class SplashScreenActivity extends AppCompatActivity {
                         Toast.makeText(SplashScreenActivity.this, "Splash Screen done", Toast.LENGTH_SHORT).show();
                     }
                 });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == LOG_IN_REQUEST_CODE)
+        {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (resultCode == RESULT_OK)
+            {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            }
+            else
+            {
+                Toast.makeText(this, "[ERROR]:"+response.getError().getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
