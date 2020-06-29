7bb3214274bb2b816f5ac47f839b03814741e0e2
Saim <saimh355@gmail.com>
2020-06-29 14:04:15 +0500

    Register Driver

--- app/src/main/java/com/example/uberjava/SplashScreenActivity.java
+++ app/src/main/java/com/example/uberjava/SplashScreenActivity.java
@@ -2,18 +2,22 @@ package com.example.uberjava;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthMethodPickerLayout;
 import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.FirebaseAuthAnonymousUpgradeException;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -26,13 +30,10 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import javax.sql.CommonDataSource;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.reactivex.Completable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.functions.Action;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -108,6 +109,42 @@ public class SplashScreenActivity extends AppCompatActivity {
     }
 
     private void showRegisterLayout() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this,R.style.DialogTheme);
+        View itemView = LayoutInflater.from(this).inflate(R.layout.layout_register,null);
+        TextInputEditText edt_first_name = (TextInputEditText)itemView.findViewById(R.id.edt_first_name);
+        TextInputEditText edt_last_name = (TextInputEditText)itemView.findViewById(R.id.edt_last_name);
+        TextInputEditText edt_phone_number = (TextInputEditText)itemView.findViewById(R.id.edt_phone_number);
+
+        Button btn_continue = (Button)itemView.findViewById(R.id.btn_continue);
+
+        //Set Data
+        if (firebaseAuth.getInstance().getCurrentUser().getPhoneNumber() != null &&
+                TextUtils.isEmpty(firebaseAuth.getInstance().getCurrentUser().getPhoneNumber()))
+        edt_phone_number.setText(firebaseAuth.getInstance().getCurrentUser().getPhoneNumber());
+
+        //Set View
+        builder.setView(itemView);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+        btn_continue.setOnClickListener(v -> {
+            if (TextUtils.isEmpty(edt_first_name.getText().toString()))
+            {
+                Toast.makeText(this, "Please enter first name", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            else if (TextUtils.isEmpty(edt_last_name.getText().toString()))
+            {
+                Toast.makeText(this, "Please enter last name", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            else if (TextUtils.isEmpty(edt_phone_number.getText().toString()))
+            {
+                Toast.makeText(this, "Please enter phone number", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+        });
 
     }
 
