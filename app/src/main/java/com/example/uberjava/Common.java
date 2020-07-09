067470e26e407762b66bce1c5a64ff88cf8dbafa
Saim <saimh355@gmail.com>
2020-07-09 17:30:47 +0500

    Manage UI Settings for ZoomMap

--- app/src/main/java/com/example/uberjava/Common.java
+++ app/src/main/java/com/example/uberjava/Common.java
@@ -5,5 +5,6 @@ import com.example.uberjava.Model.DrierInfoModel;
 public class Common {
 
     public static final String DRIVER_INFO_REFERENCE = "DriverInfo";
+    public static final String DRIVERS_LOCATION_REFERENCES = "DriversLocation";
     public static DrierInfoModel currentUser;
 }
