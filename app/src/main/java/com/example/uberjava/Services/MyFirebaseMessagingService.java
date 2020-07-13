0f0b5e6b9b97355d2afacf8180073abd090d0d1e
Saim <saimh355@gmail.com>
2020-07-13 18:58:01 +0500

    Updating Token to the server

---
+++ app/src/main/java/com/example/uberjava/Services/MyFirebaseMessagingService.java
@@ -0,0 +1,25 @@
+package com.example.uberjava.Services;
+
+import androidx.annotation.NonNull;
+
+import com.example.Utils.UserUtils;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onNewToken(@NonNull String s) {
+        super.onNewToken(s);
+        if (FirebaseAuth.getInstance().getCurrentUser() != null)
+            UserUtils.updateToken(this,s);
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+    }
+
+
+}
